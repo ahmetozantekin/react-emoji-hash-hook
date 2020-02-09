352b5b172be3d1810a1e7d7a76bb34884244566c
Ahmet Ozan Tekin <ahmetozantekin@gmail.com>
2020-02-09 22:28:20 +0300

    fixt-up

--- dist/index.es.js
+++ dist/index.es.js
@@ -1,59 +1,14 @@
 import { useState } from 'react';
 
-function _slicedToArray(arr, i) {
-  return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _nonIterableRest();
-}
+const Emojis = require('./emojis.json');
 
-function _arrayWithHoles(arr) {
-  if (Array.isArray(arr)) return arr;
-}
-
-function _iterableToArrayLimit(arr, i) {
-  if (!(Symbol.iterator in Object(arr) || Object.prototype.toString.call(arr) === "[object Arguments]")) {
-    return;
-  }
-
-  var _arr = [];
-  var _n = true;
-  var _d = false;
-  var _e = undefined;
-
-  try {
-    for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) {
-      _arr.push(_s.value);
-
-      if (i && _arr.length === i) break;
-    }
-  } catch (err) {
-    _d = true;
-    _e = err;
-  } finally {
-    try {
-      if (!_n && _i["return"] != null) _i["return"]();
-    } finally {
-      if (_d) throw _e;
-    }
-  }
-
-  return _arr;
-}
-
-function _nonIterableRest() {
-  throw new TypeError("Invalid attempt to destructure non-iterable instance");
-}
-
-var Emojis = require('./emojis.json');
-
-var useHashEmoji = function useHashEmoji(str) {
-  var _useState = useState(str),
-      _useState2 = _slicedToArray(_useState, 1),
-      strState = _useState2[0];
-
+const useHashEmoji = str => {
+  let [strState] = useState(str);
   var hashedString = hashCode(strState);
   return convertToEmoji(hashedString);
 };
 
-var hashCode = function hashCode(str) {
+const hashCode = str => {
   if (Array.prototype.reduce) {
     return str.split("").reduce(function (a, b) {
       a = (a << 5) - a + b.charCodeAt(0);
@@ -73,22 +28,21 @@ var hashCode = function hashCode(str) {
   return hash;
 };
 
-var convertToEmoji = function convertToEmoji(hStr) {
-  var hashLength = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 4;
-  var hexHash = hexEncode(hStr.toString());
-  var decimalHash = parseInt(hexHash, 16);
-  var emojiIndex = decimalHash % Math.pow(Emojis.length, hashLength);
-  var emojiString = '';
+const convertToEmoji = (hStr, hashLength = 4) => {
+  const hexHash = hexEncode(hStr.toString());
+  const decimalHash = parseInt(hexHash, 16);
+  let emojiIndex = decimalHash % Math.pow(Emojis.length, hashLength);
+  let emojiString = '';
 
-  for (var ii = 0; ii < hashLength; ii++) {
-    emojiString = "".concat(Emojis[emojiIndex % Emojis.length]).concat(emojiString);
+  for (let ii = 0; ii < hashLength; ii++) {
+    emojiString = `${Emojis[emojiIndex % Emojis.length]}${emojiString}`;
     emojiIndex = Math.floor(emojiIndex / Emojis.length);
   }
 
   return emojiString;
 };
 
-var hexEncode = function hexEncode(str) {
+const hexEncode = str => {
   var hex, i;
   var result = "";
 
